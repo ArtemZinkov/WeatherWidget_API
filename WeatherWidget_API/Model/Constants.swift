5e6eb6484f6b7f98a61549c256e70bc83eaf5a62
Artem <artem@artems-mac.local>
2018-02-12 12:20:13 -0800

    Виділив окремо константи

---
+++ WeatherWidget_API/Model/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  
+//
+//  Created by Artem on 04.02.2018.
+//
+
+import Foundation
+import CoreLocation
+
+struct Constants {
+    static let KELVIN_CONSTANT = 273.15
+    static let APIAdress = "http://api.openweathermap.org/data/2.5/weather"
+    static let APIkeys =
+    [
+    "ce1386c86a0219f660c0b2820610c9ff",
+    "d7dd893f6ba991f650de2d6b69c2f1fa"
+    ]
+}
